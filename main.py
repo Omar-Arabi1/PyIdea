c6f4c417c4665cdd6649ae2c4b5f0f9943ebb7c9
Omar-Arabi1 <omarbuisness7@gmail.com>
2025-06-24 18:17:17 +0300

    updated the documentation

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from colorama import Fore, init
 import os
 import pathlib
 
+# we use this to start colorama to color the output
 init()
 
+# we create the app
 app = typer.Typer()
 
 """
@@ -21,9 +23,6 @@ GoProjects:
     link: /home/omar-arabi/obsidian/CLIGo.md
 """
 
-# header -> link (optional) -> sub-folder
-
-
 # loading/reading the json file containing the ideas and their destinations
 def read_json():
     with open("Data/idea.json", mode="r", encoding="utf-8") as read_file:
@@ -35,12 +34,16 @@ def write_json(dict_to_json):
         return dump(dict_to_json, write_file, indent=4)
     
 def check_if_exists(s_folder: str, idea_json: dict):
+    # we check if there are any sub-folders to check for the links inside of them
     if len(idea_json.keys()) != 0 and s_folder.upper() in idea_json.keys():
-        keys = list(idea_json[s_folder.upper()].keys())
+        keys = list(idea_json[s_folder.upper()].keys()) # we take the sub-folders out so that we could change each link separately
         for i, l in enumerate(idea_json[s_folder.upper()].values()):
-            if not os.path.isfile(l) and l != "":
+            # we check if it exists and is a file and also if it isn't empty 
+            # because that would give error messages for no reason and also check the extension
+            if not os.path.isfile(l) and l != "" and not pathlib.Path(l).suffix != ".md":
+                # if it is not found we make it empty and notify the user
                 print(Fore.RED + f"{l} got deleted or changed its absolute so it got removed")
-                idea_json[s_folder.upper()][keys[i]] = ""
+                idea_json[s_folder.upper()][keys[i]] = "" 
                 write_json(idea_json)
 
 """
@@ -52,7 +55,7 @@ TODO:
 @app.command() 
 def add_idea(sub_folder: str, idea: str, link: str = ""):
     ideas_json: dict = read_json()
-    check_if_exists(sub_folder, ideas_json)
+    check_if_exists(sub_folder, ideas_json) # we always run this to always check for if links are missed
 
     # by splitting the values on whitespace doesn't allow the user to enter a bunch of spaces 
     # which would work if we compared it to an empty string since a whitespace spammed one isn't considered empty
@@ -67,6 +70,7 @@ def add_idea(sub_folder: str, idea: str, link: str = ""):
                 print(Fore.RED + "idea/link already exists dumbass")
                 return
 
+    # we chceck if its a file and if it is a markdown file
     if not os.path.isfile(link) and link != "":
         print(Fore.RED + f"{link} doesn't exist or is a directory")
         return 
@@ -74,11 +78,14 @@ def add_idea(sub_folder: str, idea: str, link: str = ""):
         print(Fore.RED + f"{link} must be in markdown")
         return 
 
+    # if there is a sub-folder and the user chose it we update it if not we create it
     if sub_folder.upper() in ideas_json.keys():
         ideas_json[sub_folder.upper()].update({ idea: link }) # we make the sub-folder uppercase don't ask me why its fun
     else:
         ideas_json.update({ sub_folder.upper(): { idea: link } })
+
     write_json(ideas_json)
 
+# run the file as a python binary not as typer's
 if __name__ == "__main__":
     app()
